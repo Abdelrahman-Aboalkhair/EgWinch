ffb267549cde0f7cc70f804363ec113f1cde2763
Abdelrahman Aboalkhair <bgbody5@gmail.com>
2025-02-19 05:15:06 +0200

    At this state the conversations comopnent is all working without the mark_as_read event, added ConversationSlice

--- client/app/StoreProvider.tsx
+++ client/app/StoreProvider.tsx
@@ -12,22 +12,22 @@ export default function StoreProvider({
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    const user = localStorage.getItem("user");
+  // useEffect(() => {
+  //   const user = localStorage.getItem("user");
 
-    try {
-      if (user) {
-        store.dispatch(
-          setCredentials({ data: { user: JSON.parse(user), accessToken: "" } })
-        );
-      } else {
-        store.dispatch(clearAuthState());
-      }
-    } catch (error) {
-      console.error("Error parsing user from localStorage", error);
-      store.dispatch(clearAuthState());
-    }
-  }, []);
+  //   try {
+  //     if (user) {
+  //       store.dispatch(
+  //         setCredentials({ data: { user: JSON.parse(user), accessToken: "" } })
+  //       );
+  //     } else {
+  //       store.dispatch(clearAuthState());
+  //     }
+  //   } catch (error) {
+  //     console.error("Error parsing user from localStorage", error);
+  //     store.dispatch(clearAuthState());
+  //   }
+  // }, []);
 
   return <Provider store={store}>{children}</Provider>;
 }
